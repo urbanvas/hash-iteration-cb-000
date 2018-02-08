9724fda57c9beff8d874880ba47b23a2f4a0f756
Neil Seejoor <nvseejoor@gmail.com>
2018-02-08 21:07:05 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,12 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+  def happy_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
   end
-end
-
-winner  
 end
